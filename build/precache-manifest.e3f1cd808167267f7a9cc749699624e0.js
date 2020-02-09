551ebbca0985d38d3962c10d4ef57b9f70f64b1a
Florian Ipate <florianipate@Florians-iMac.local>
2020-02-09 19:29:55 +0000

    adding file to github

---
+++ build/precache-manifest.e3f1cd808167267f7a9cc749699624e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f5effe242f46ea2ba8d4d473f86bfd",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6cf8d3f78d5a2a7e6aca",
+    "url": "/monsters/static/css/main.ed5f0a6a.chunk.css"
+  },
+  {
+    "revision": "4beba386462ee8d71d1c",
+    "url": "/monsters/static/js/2.a099693a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.a099693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf8d3f78d5a2a7e6aca",
+    "url": "/monsters/static/js/main.4ca9ba89.chunk.js"
+  },
+  {
+    "revision": "255868a8cfcf2a3f9793",
+    "url": "/monsters/static/js/runtime-main.b78649e3.js"
+  }
+]);
